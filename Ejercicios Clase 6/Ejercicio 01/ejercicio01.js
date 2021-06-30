e28d09ec0ff7a067e7b020bc8015324d4f97b3f1
crisanmet <cristiansancricca@gmail.com>
2021-06-29 23:37:42 -0300

    Se identa para que el codigo sea mas legible
    Identacion del html.

--- Ejercicios Clase 6/Ejercicio 01/ejercicio01.js
+++ Ejercicios Clase 6/Ejercicio 01/ejercicio01.js
@@ -1,141 +1,186 @@
+document.querySelector("#boton-siguiente").onclick = function () {
+  const $cantidadDeIntegrantes = Number(
+    document.querySelector("#cantidad-integrantes").value
+  );
 
+  borrarIntegrantesCargados();
+  crearIntegrantes($cantidadDeIntegrantes);
 
-document.querySelector("#boton-siguiente").onclick = function(){
+  return false;
+};
 
-    const $cantidadDeIntegrantes = Number(document.querySelector("#cantidad-integrantes").value);  
-    
-    borrarIntegrantesCargados();
-    crearIntegrantes($cantidadDeIntegrantes);
-    
+document.querySelector("#calcular").onclick = function (event) {
+  const edades = edadesIntegrantes();
+  const validarForm = validarFormulario(edades);
 
-    return false;
-}
+  if (validarForm) {
+    const $errores = document.querySelector("#errores");
+    const $error = document.createElement("li");
+    $error.innerText = validarForm;
 
-document.querySelector("#calcular").onclick = function(){
-const edades=edadesIntegrantes();
+    $errores.appendChild($error);
 
-const edadMayor= numeroMayor(edades);
-const edadMenor= numeroMenor(edades);
-const promedioEdad= sacarPromedio(edades);
+    mostrarErrores();
+    ocultarResultados();
+    mostrarReset();
+  } else {
+    const edadMayor = numeroMayor(edades);
+    const edadMenor = numeroMenor(edades);
+    const promedioEdad = sacarPromedio(edades);
 
-document.querySelector("#edad-mayor").innerText= edadMayor;
-document.querySelector("#edad-menor").innerText= edadMenor;
-document.querySelector("#edad-promedio").innerText=promedioEdad;
-mostrarResultados();
-mostrarReset();
+    document.querySelector("#edad-mayor").innerText = edadMayor;
+    document.querySelector("#edad-menor").innerText = edadMenor;
+    document.querySelector("#edad-promedio").innerText = promedioEdad;
+    mostrarResultados();
+    mostrarReset();
+    ocultarErrores();
 
-}
+    event.preventDefault();
+  }
+};
 
 document.querySelector("#resetear").onclick = reset;
 
+function crearIntegrantes(integrantes) {
+  if (integrantes > 0) {
+    mostrarBtnCalcular();
+  } else {
+    reset();
+  }
 
-
-function crearIntegrantes(integrantes){
-    if(integrantes >0){
-        mostrarBtnCalcular();
-    }else {
-        reset();
-    }
-    
-    for(let i=0; i <integrantes;i++){
-        mostrarIntegrantes(i);
-    }
-
+  for (let i = 0; i < integrantes; i++) {
+    mostrarIntegrantes(i);
+  }
 }
 
-function mostrarIntegrantes(numero){
-    const elementoDiv = document.createElement("div");
-    elementoDiv.className="integrante";
-    
+function mostrarIntegrantes(numero) {
+  const elementoDiv = document.createElement("div");
+  elementoDiv.className = "integrante";
 
-    const elementoLabel=document.createElement("label");
-    elementoLabel.textContent=`Edad del integrante Nro. ${numero + 1}`
-    const elementoInput=document.createElement("input");
-    elementoInput.type="number";
+  const elementoLabel = document.createElement("label");
+  elementoLabel.textContent = `Edad del integrante Nro. ${numero + 1}`;
+  const elementoInput = document.createElement("input");
+  elementoInput.type = "number";
 
-    elementoDiv.appendChild(elementoLabel);
-    elementoDiv.appendChild(elementoInput);
+  elementoDiv.appendChild(elementoLabel);
+  elementoDiv.appendChild(elementoInput);
 
-    const $integrantes= document.querySelector("#cargar-integrantes");
-    $integrantes.appendChild(elementoDiv);
-
-
-
+  const $integrantes = document.querySelector("#cargar-integrantes");
+  $integrantes.appendChild(elementoDiv);
 }
 
-function borrarIntegrantesCargados(){
-
-    const $integrantes=document.querySelectorAll(".integrante");
-        for(let i=0; i< $integrantes.length;i ++){
-          $integrantes[i].remove();  
-        }
-
-}
-function mostrarBtnCalcular(){
-    document.querySelector("#calcular").className="";
+function borrarIntegrantesCargados() {
+  const $integrantes = document.querySelectorAll(".integrante");
+  for (let i = 0; i < $integrantes.length; i++) {
+    $integrantes[i].remove();
+  }
 }
 
-function ocultarBtnCalcular(){
-    document.querySelector('#calcular').className = "oculto";
+function borrarErrores() {
+  const errores = document.querySelectorAll("#errores");
+  for (let i = 0; i < errores.length; i++) {
+    errores[i].remove();
+  }
+}
+function mostrarBtnCalcular() {
+  document.querySelector("#calcular").className = "";
+}
+
+function ocultarBtnCalcular() {
+  document.querySelector("#calcular").className = "oculto";
 }
 
 function ocultarResultados() {
-    document.querySelector('#calculo-edades').className = "oculto";
-  }
-
-  function mostrarResultados() {
-    document.querySelector('#calculo-edades').className = "";
-  }
-
-  function mostrarReset(){
-    document.querySelector("#resetear").className="";
-  }
-
-  function ocultarReset(){
-    document.querySelector("#resetear").className="oculto";
-  }
-
-
-  function edadesIntegrantes() {
-    const $integrantes = document.querySelectorAll(".integrante input");
-    const edades = [];
-    for (let i = 0; i < $integrantes.length; i++) {
-      edades.push(Number($integrantes[i].value));
-    }
-    return edades;
-  }
-
-  function reset(){
-    borrarIntegrantesCargados();
-    ocultarResultados();
-    ocultarBtnCalcular();
-    ocultarReset();
-
-  }
-
-  function numeroMenor(numero){
-    let menor=numero[0];
-    for(let i=0; i<numero.length; i++){
-        if(numero[i] < menor){
-            menor=numero[i];
-    }
-    }
-    return menor;
+  document.querySelector("#calculo-edades").className = "oculto";
 }
 
-function numeroMayor(numero){
-    let mayor= numero[0];
-    for(let i=0; i<numero.length; i++){
-        if (numero[i] > mayor){
-            mayor=numero[i];
-        }
-    }return mayor;
+function mostrarResultados() {
+  document.querySelector("#calculo-edades").className = "";
 }
 
+function mostrarReset() {
+  document.querySelector("#resetear").className = "";
+}
 
-function sacarPromedio(numero){
-    let contador = 0;
-    for(let i=0; i<numero.length; i++){
-        contador += numero[i];
-    } return contador / numero.length;
+function ocultarReset() {
+  document.querySelector("#resetear").className = "oculto";
+}
+
+function mostrarErrores() {
+  document.querySelector("#errores").className = "";
+}
+
+function ocultarErrores() {
+  document.querySelector("#errores").className = "oculto";
+}
+
+function edadesIntegrantes() {
+  const $integrantes = document.querySelectorAll(".integrante input");
+  const edades = [];
+  for (let i = 0; i < $integrantes.length; i++) {
+    edades.push(Number($integrantes[i].value));
+  }
+  return edades;
+}
+
+function validarFormulario(edades) {
+  const edad = [];
+  let edadIteracion;
+  let validarEdades;
+  let errores = [];
+
+  for (let i = 0; i < edades.length; i++) {
+    edadIteracion = edades[i].value;
+    validarEdades = validarEdad(edadIteracion);
+    if (validarEdades !== 0) {
+      errores.push(validarEdades);
+    }
+  }
+  return errores;
+}
+
+function validarEdad(numero) {
+  if (!/[0-9\.]+$/.test(numero)) {
+    return "Este campo solo acepta números";
+  }
+  if (numero < 1) {
+    return "Debe ser mayor a $0";
+  }
+  return "";
+}
+
+function reset() {
+  borrarIntegrantesCargados();
+  ocultarResultados();
+  ocultarBtnCalcular();
+  ocultarReset();
+  borrarErrores();
+}
+
+function numeroMenor(numero) {
+  let menor = numero[0];
+  for (let i = 0; i < numero.length; i++) {
+    if (numero[i] < menor) {
+      menor = numero[i];
+    }
+  }
+  return menor;
+}
+
+function numeroMayor(numero) {
+  let mayor = numero[0];
+  for (let i = 0; i < numero.length; i++) {
+    if (numero[i] > mayor) {
+      mayor = numero[i];
+    }
+  }
+  return mayor;
+}
+
+function sacarPromedio(numero) {
+  let contador = 0;
+  for (let i = 0; i < numero.length; i++) {
+    contador += numero[i];
+  }
+  return contador / numero.length;
 }
